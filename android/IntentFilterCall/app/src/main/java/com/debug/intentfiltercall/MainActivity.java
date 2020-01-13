e0187e8596e0b0f8d1cf1f2146fc2bd7e11e3f52
yaonimingsanqian <529670074@qq.com>
2020-01-13 20:57:01 +0800

    intentFilter

---
+++ android/IntentFilterCall/app/src/main/java/com/debug/intentfiltercall/MainActivity.java
@@ -0,0 +1,31 @@
+package com.debug.intentfiltercall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("com.ispring.action.ACTION_TEST1");
+                Uri uri = Uri.parse("custome://gan.com");
+                intent.setData(uri);
+                Log.d("MainActivity",intent.toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
